6372a51810f8d35dd49897b2fc84b21af5bba682
谢骐 <shynur@outlook.com>
2026-03-26 15:30:10 +0800

    init 骨架

--- utilities/bip32/index.mjs
+++
@@ -1,3 +0,0 @@
-export {
-
-} from './bip32.mjs'
---
+++ utilities/bip32/secp256k1.mjs
@@ -0,0 +1,21 @@
+/*
+ * 提供 BIP 32 所需的 secp256k1 相关 API.
+ * Copyright (C) 2026  谢骐 <shynur@outlook.com>.  All Rights Reserved.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+/*
+ * Overview:
+ *   本 module 遵循论文 SEC 2 v2 <https://www.secg.org/sec2-v2.pdf> 的命名约定, 提供同名的 API.
+ *   具体实现方式是对现成的实现进行封装, 但 API 应与现成的实现完全解耦 (i.e., 即使更换依赖的实现也不影响 API).
+ *
+ * 依赖关系:
+ *   不依赖任何第三方库或宿主特定的 API, 仅使用 JavaScript 标准库, noble/secp256k1, 或 Web API.
+ *
+ * Quality:
+ *   类型注解使用 JSDoc, 可用 tsc 检查.
+ */
+
+import {
+
+} from './noble-secp256k1.mjs'
