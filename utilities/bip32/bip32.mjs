f35e1e9a2331cca60796d5308b308ab37a89afc1
谢骐 <shynur@outlook.com>
2026-03-26 16:23:26 +0800

    base58

--- utilities/bip32/bip32.mjs
+++ utilities/bip32/bip32.mjs
@@ -6,7 +6,7 @@
  */
 /*
  * Overview:
- *   // TODO
+ *   TODO
  *
  * 依赖关系:
  *   本文件不依赖任何第三方库或宿主特定的 API, 仅使用 JavaScript 标准库, 当前目录的 modules, 或 Web API.
@@ -18,3 +18,28 @@
 import {
 
 } from './secp256k1.mjs'
+
+/**
+ * base58 编码
+ * @param {Uint8Array} bytes
+ * @returns {string}
+ */
+function base58Encode(bytes) {
+    const ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+    let leadingZeros = 0
+    for (let i = 0; i < bytes.length && bytes[i] == 0; ++i)
+        ++leadingZeros
+
+    let num = 0n
+    for (const byte of bytes)
+        num = num * 256n + BigInt(byte)
+
+    let result = ''
+    while (num > 0n) {
+        result = ALPHABET[Number(num % 58n)] + result
+        num /= 58n
+    }
+
+    return '1'.repeat(leadingZeros) + result
+}
